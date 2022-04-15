4ceb27e938b6794cdb5e6efd419f9cde4e5a85e9
Tanmay00007 <tanmayc2001@gmail.com>
2022-04-15 12:58:42 +0530

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let box = document.getElementById('box');
+let svalue = '';
+b1 = document.querySelectorAll('button');
+for(x of b1){
+    x.addEventListener('click',(e)=>{
+        btext = e.target.innerText;
+        if(btext=='X'){
+            btext = '*';
+            svalue += btext;
+            box.value = svalue;
+        }
+        else if(btext == 'C'){
+            svalue='';
+            box.value = svalue;
+        }
+        else if(btext == '='){
+            box.value = eval(svalue);
+        }
+        else{
+            svalue += btext;
+            box.value = svalue; 
+        }
+    })
+}
